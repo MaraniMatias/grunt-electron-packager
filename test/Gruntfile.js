f70aeaffb3d681a087321f1322b641a65d808c2e
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-04-21 13:32:57 -0300

    Add new tests

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -6,16 +6,34 @@ module.exports = function (grunt) {
       options: {
         asar: true,
         dir: './app',
-        icon: './app/recursos/icon',
         ignore: 'bower.json',
         out: './build',
         overwrite: true
       },
+      buildLinux: {
+        name: 'nameBuild-test-linux',
+        arch: 'x64',
+        platform: 'linux',
+      },
+      buildWin: {
+        name: 'nameBuild-test-win',
+        arch: 'x64',
+        platform: 'win32',
+      },
       build: {
-        arch: os.arch(),
-        electronVersion: '1.8.4', // set version of electron
-        name: 'nameBuild-test',
-        platform: os.platform(),
+        options:{
+          platform        : os.platform(),
+          arch            : os.arch(),
+          dir             : './app',
+          out             : './build',
+          icon            : './app/recursos/icon',
+          name            : 'nameBuild',
+          ignore          : 'bower.json',
+          // set specific version of electron, If it isn't using the electron's version on your deps.
+          electronVersion: '1.8.4',
+          asar      : true,
+          overwrite : true
+        }
       },
       buildCustom: {
         options: function (name, platform, arch) {
@@ -37,6 +55,8 @@ module.exports = function (grunt) {
   grunt.loadTasks('./../tasks');
   grunt.registerTask('default', [
     'electron-packager:build',
+    'electron-packager:buildLinux',
+    'electron-packager:buildWin',
     'electron-packager:buildCustom:buildCustomName:win32:x64'
   ]);
 };
